c113ef54a04d98a5698d29400dc369935e978907
zerospeedzero <george.cheng@eauclairepartners.com>
2025-02-12 21:02:10 -0700

    Ignore access right

--- backend/src/graphDB_dataAccess.py
+++ backend/src/graphDB_dataAccess.py
@@ -160,39 +160,40 @@ class graphDBdataAccess:
             logging.info("Vector index does not exist, So KNN graph not update")
 
     def check_account_access(self, database):
-        try:
-            query_dbms_componenet = "call dbms.components() yield edition"
-            result_dbms_componenet = self.graph.query(query_dbms_componenet)
-
-            if  result_dbms_componenet[0]["edition"] == "enterprise":
-                query = """
-                SHOW USER PRIVILEGES 
-                YIELD * 
-                WHERE graph = $database AND action IN ['read'] 
-                RETURN COUNT(*) AS readAccessCount
-                """
+        return True
+        # try:
+        #     return True
+        #     query_dbms_componenet = "call dbms.components() yield edition"
+        #     result_dbms_componenet = self.graph.query(query_dbms_componenet)
+            # if  result_dbms_componenet[0]["edition"] == "enterprise":
+            #     query = """
+            #     SHOW USER PRIVILEGES 
+            #     YIELD * 
+            #     WHERE graph = $database AND action IN ['read'] 
+            #     RETURN COUNT(*) AS readAccessCount
+            #     """
             
-                logging.info(f"Checking access for database: {database}")
+            #     logging.info(f"Checking access for database: {database}")
 
-                result = self.graph.query(query, params={"database": database})
-                read_access_count = result[0]["readAccessCount"] if result else 0
+            #     result = self.graph.query(query, params={"database": database})
+            #     read_access_count = result[0]["readAccessCount"] if result else 0
 
-                logging.info(f"Read access count: {read_access_count}")
+            #     logging.info(f"Read access count: {read_access_count}")
 
-                if read_access_count > 0:
-                    logging.info("The account has read access.")
-                    return False
-                else:
-                    logging.info("The account has write access.")
-                    return True
-            else:
-                #Community version have no roles to execute admin command, so assuming write access as TRUE
-                logging.info("The account has write access.")
-                return True
+            #     if read_access_count > 0:
+            #         logging.info("The account has read access.")
+            #         return False
+            #     else:
+            #         logging.info("The account has write access.")
+            #         return True
+            # else:
+            #     #Community version have no roles to execute admin command, so assuming write access as TRUE
+            #     logging.info("The account has write access.")
+            #     return True
 
-        except Exception as e:
-            logging.error(f"Error checking account access: {e}")
-            return False
+        # except Exception as e:
+            # logging.error(f"Error checking account access: {e}")
+            # return False
 
     def check_gds_version(self):
         try:
